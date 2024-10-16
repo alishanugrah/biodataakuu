752bdb73b77a55f0be48258d655d068734f73490
4lishanugrah <alishananugrah@gmail.com>
2024-10-16 20:54:14 +0800

    Update biodata.js

--- biodata.js
+++ biodata.js
@@ -5,11 +5,6 @@ const isLoggedIn = localStorage.getItem('isLoggedIn');
 if (isLoggedIn !== 'true') {
     window.location.href = "index.html"; // Ganti dengan nama file login yang sesuai
 }
-
-// Jika pengguna login, tampilkan pesan selamat datang
-document.getElementById('message').textContent = 'Selamat datang di halaman biodata!';
-document.getElementById('message').style.color = 'green'; // Ubah warna pesan menjadi hijau
-
 // Fungsi untuk logout
 function logout() {
     const confirmLogout = confirm("Apakah Anda yakin ingin logout?");
